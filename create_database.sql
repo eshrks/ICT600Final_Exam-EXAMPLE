22fd5ae35d1b7ec04b5584c74d15ab080aa8dc08
eshrks <103642209+eshrks@users.noreply.github.com>
2023-12-06 02:54:26 +0800

    Add files via upload

---
+++ create_database.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE students_namaewa;
+
+USE students_namaewa;
+
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	userLevel VARCHAR(15),
+	lastName VARCHAR(50),
+	firstName VARCHAR(50),
+	middleName VARCHAR(50),
+	contactNumber VARCHAR(13),
+	userName VARCHAR(50),
+	pword VARCHAR(250),
+	dateCreated DATE,
+	isDeleted INT(1) DEFAULT 0
+);
+
+CREATE TABLE students(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	userLevel VARCHAR(15),
+	studentNumber VARCHAR(13),
+	fullName VARCHAR(250),
+	midtermGrade DECIMAL(5, 2),
+	finalGrade DECIMAL(5, 2),
+	dateCreated DATE,
+	isDeleted INT(1) DEFAULT 0
+);
